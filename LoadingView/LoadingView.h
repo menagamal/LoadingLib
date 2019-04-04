5ba112fd2025d947120fddc112740e0fad3e6243
Mena Gamal <>
2019-04-04 14:38:29 +0200

    first commit

---
+++ LoadingView/LoadingView.h
@@ -0,0 +1,19 @@
+//
+//  LoadingView.h
+//  LoadingView
+//
+//  Created by Mena Gamal on 4/4/19.
+//  Copyright © 2019 Mena Gamal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoadingView.
+FOUNDATION_EXPORT double LoadingViewVersionNumber;
+
+//! Project version string for LoadingView.
+FOUNDATION_EXPORT const unsigned char LoadingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingView/PublicHeader.h>
+
+
---
+++ LoadingViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingViewExample
+//
+//  Created by Mena Gamal on 4/4/19.
+//  Copyright © 2019 Mena Gamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
